a4ab7db863172e99d3ca2ae201b6c5cd345cd1ca
Aditya Shinde <adityashinde@Adityas-MacBook-Air-2.local>
2023-03-17 14:22:44 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
+import vuetify from './plugins/vuetify'
+import GlobalMixins from '@/utils/GlobalMixins.vue'
+import AppConstants from '@/utils/AppConstants.js'
 
 Vue.config.productionTip = false
+Vue.mixin(GlobalMixins)
+Vue.prototype.$const = AppConstants
 
 new Vue({
   router,
-  store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="center">  
+        <v-row>
+            <v-col cols="10" offset="1">
+                <v-avatar color="primary" size="95" tile>
+                    <v-img src="@/assets/logo.jpg"></v-img>
+                </v-avatar>
+                <v-text-field label="username" v-model="username" outlined></v-text-field>
+                <v-text-field label="Password" v-model="Password" type="password" outlined></v-text-field>
+                <v-btn elevation="2" @click="authenticate()">Login</v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: "", 
+                password: ""
+            }
+            
+        },
+        methods: {
+
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
